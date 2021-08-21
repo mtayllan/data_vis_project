10c0ceceb69de18fef023409b5e263d2a0e4a707
mtayllan <messiastayllan@gmail.com>
2021-08-21 17:14:40 -0300

    Improve series chart performance

--- charts/series.js
+++ charts/series.js
@@ -5,13 +5,12 @@
   const facts = crossfilter(dataset)
   const dateDim = facts.dimension(d => d.Date)
   const xScale = d3.scaleTime().domain([dateDim.bottom(1)[0].Date, dateDim.top(1)[0].Date])
-  const languageColorScale = d3.scaleOrdinal(d3.schemePaired);
 
   const selectedLanguages = languages.sort(() => Math.random() - Math.random()).slice(0, 3);
 
   languages.sort();
 
-  const width = 1000;
+  const width = 600;
 
   const removeLanguage = (lang) => {
     const index = selectedLanguages.indexOf(lang);
@@ -20,24 +19,33 @@
     }
   }
 
-  const build = () => {
-    const compositeChart = dc.compositeChart(document.querySelector("#series"))
-
-    const lineCharts = selectedLanguages.map((language, i) => (
-      dc.lineChart(compositeChart)
-        .group(dateDim.group().reduceSum(d => d[language]), language)
-        .ordinalColors([defaultColors[i]])
-    ));
-
-    compositeChart.width(width)
+  const compositeChart = dc.compositeChart(document.querySelector("#series"));
+  compositeChart.width(width)
       .height(400)
       .margins({ top: 50, right: 50, bottom: 25, left: 30 })
       .dimension(dateDim)
       .x(xScale)
       .xUnits(d3.timeDays)
+      .y(d3.scaleLinear().domain([0, 33]))
       .renderHorizontalGridLines(true)
       .legend(dc.legend().x(width - 200).y(5).itemHeight(13).gap(5))
       .brushOn(false)
+      .compose([]);
+
+  const build = () => {
+    const tops = [];
+    const lineCharts = selectedLanguages.map((language, i) => {
+      const group = dateDim.group().reduceSum(d => d[language]);
+      tops.push(group.top(1)[0].value);
+      return dc.lineChart(compositeChart)
+        .group(group, language)
+        .ordinalColors([defaultColors[i]])
+    });
+
+    const topsSorted = tops.sort((a, b) => b - a);
+
+    compositeChart
+      .y(d3.scaleLinear().domain([0, topsSorted[0]]))
       .compose(lineCharts)
 
     dc.renderAll()
@@ -76,13 +84,10 @@
     .attr('class', 'form-check-label')
     .text(d => d)
 
-  const selectInitialLanguages = (languages) => {
-    languages.map((lang) => {
-      document.querySelectorAll(`input[type='checkbox'][value='${lang}']`)[0].checked = true;
-    })
-  }
+  selectedLanguages.map((lang) => {
+    document.querySelectorAll(`input[type='checkbox'][value='${lang}']`)[0].checked = true;
+  })
 
-  selectInitialLanguages(selectedLanguages);
   build();
 
 })();
